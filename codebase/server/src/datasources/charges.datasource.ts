a805ff75801e756881ab11e4be044ded1ce4798f
Daniel Bound <boun.d@hotmail.com>
2025-05-03 20:25:10 +1000

    [master] (Daniel) Charges datasource using mock

---
+++ codebase/server/src/datasources/charges.datasource.ts
@@ -0,0 +1,39 @@
+import { RESTDataSource, AugmentedRequest } from "@apollo/datasource-rest";
+import { MOCK_DUE_CHARGES_API } from "./__mocks__/dueChargesAPIMock";
+
+export interface Charge {
+  id: string;
+  accountId: string;
+  date: string;
+  amount: number;
+}
+
+export class ChargesDataSource extends RESTDataSource {
+  token: string;
+
+  constructor(baseURL: string, token: string) {
+    super();
+    this.baseURL = baseURL;
+    this.token = token;
+  }
+
+  override willSendRequest(_path: string, request: AugmentedRequest) {
+    request.headers["Authorization"] = `Bearer ${this.token}`;
+  }
+
+  async getCharges(): Promise<Charge[]> {
+    return MOCK_DUE_CHARGES_API().catch((e) => {
+      //handle error
+      throw e;
+    });
+  }
+
+  async getCharge(id: string): Promise<Charge | undefined> {
+    return MOCK_DUE_CHARGES_API()
+      .then((charges) => charges.find((charge) => charge.id === id))
+      .catch((e) => {
+        //handle error
+        throw e;
+      });
+  }
+}
